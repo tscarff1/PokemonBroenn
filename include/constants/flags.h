dd4e5f0cd431af92375a37785155211bf52c3d74
Tyler Scarff <tscarff1@gmail.com>
2022-03-31 10:58:17 -0400

    Added egg giver TOGEPI in Petalburg

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1502,7 +1502,7 @@
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
 #define FLAG_RECEIVED_OLDALE_EGG                    (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_RECEIVED_PETALBURG_EGG                 (SYSTEM_FLAGS + 0x86) // Unused Flag
 #define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
