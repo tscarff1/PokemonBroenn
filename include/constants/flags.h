eee959f72bc03db717bf892804215e79d2a1a83a
Tyler Scarff <tscarff1@gmail.com>
2022-04-10 10:44:18 -0400

    Implemented MAuville Egg giver

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1503,7 +1503,7 @@
 
 #define FLAG_RECEIVED_OLDALE_EGG                    (SYSTEM_FLAGS + 0x85) // Unused Flag
 #define FLAG_RECEIVED_PETALBURG_EGG                 (SYSTEM_FLAGS + 0x86) // Unused Flag
-#define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
+#define FLAG_RECEIVED_MAUVILLE_EGG                  (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
 #define FLAG_UNUSED_0x8EA                           (SYSTEM_FLAGS + 0x8A) // Unused Flag
