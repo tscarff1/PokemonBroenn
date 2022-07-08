55dfaa46d7a5d0a4e4beb98c44df0ee07fc66b87
Tyler Scarff <tscarff1@gmail.com>
2022-07-08 17:29:19 -0400

    completed graphical changes for sootopolis gym

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -437,6 +437,7 @@ const u8 gInitialMovementTypeFacingDirections[] = {
 #define OBJ_EVENT_PAL_TAG_RS_BRENDAN              0x1122
 #define OBJ_EVENT_PAL_TAG_RS_MAY                  0x1123
 #define OBJ_EVENT_PAL_TAG_JIRACHI                 0x1124
+#define OBJ_EVENT_PAL_TAG_LEADER_TYLER            0x1125
 #define OBJ_EVENT_PAL_TAG_NONE                    0x11FF
 
 #include "data/object_events/object_event_graphics_info_pointers.h"
@@ -484,6 +485,7 @@ static const struct SpritePalette sObjectEventSpritePalettes[] = {
     {gObjectEventPal_RubySapphireBrendan,   OBJ_EVENT_PAL_TAG_RS_BRENDAN},
     {gObjectEventPal_RubySapphireMay,       OBJ_EVENT_PAL_TAG_RS_MAY},
     {gObjectEventPal_Jirachi,               OBJ_EVENT_PAL_TAG_JIRACHI},
+    {gObjectEventPal_Tyler,                 OBJ_EVENT_PAL_TAG_LEADER_TYLER},
     {},
 };
 
