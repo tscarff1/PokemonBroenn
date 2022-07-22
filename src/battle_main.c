e610bc22cce8a5392123bf79dc5432c66c5b44c3
Tyler Scarff <tscarff1@gmail.com>
2022-07-21 22:56:42 -0400

    Jill is a now a King Trainer class

--- src/battle_main.c
+++ src/battle_main.c
@@ -385,6 +385,7 @@ const struct TrainerMoney gTrainerMoneyTable[] =
     {TRAINER_CLASS_HIKER, 10},
     {TRAINER_CLASS_YOUNG_COUPLE, 8},
     {TRAINER_CLASS_WINSTRATE, 10},
+    {TRAINER_CLASS_KING, 50},
     {0xFF, 5}, // Any trainer class not listed above uses this
 };
 
